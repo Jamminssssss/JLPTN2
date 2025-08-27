4a1555f2be48a8472c10872c1fa19ffec4a09ced
박재민 <benibara3758@gmail.com>
2025-08-27 13:25:03 +0900

    Initial Commit

---
+++ N2TestApp/N2TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  N2TestAppApp.swift
+//  N2TestApp
+//
+//  Created by JeaminPark on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct N2TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
